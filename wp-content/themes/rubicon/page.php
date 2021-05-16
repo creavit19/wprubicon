c96e20471fe0c94bbba24b0beeb390c005937804
Volodimir Ilinskiy <creavit@i.ua>
2021-05-16 21:05:07 +0300

    ADDED | ACF plugin

--- wp-content/themes/rubicon/page.php
+++ wp-content/themes/rubicon/page.php
@@ -3,7 +3,7 @@
       <?php while ( have_posts() ) : the_post(); ?>
 			<article <?php post_class() ?> >
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-
+        <?php the_post_thumbnail(); ?>
 				<div class="entry-content">
 					<?php the_content(); ?>
 					<?php edit_post_link( __( 'Edit', 'rubicon' ), '<span class="edit-link">', '</span>' ); ?>
